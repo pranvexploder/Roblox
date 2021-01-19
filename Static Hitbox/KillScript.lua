5d7fc051a05004c0e61e549a592cbcfe0b99895b
pranvexploder <56198908+pranvexploder@users.noreply.github.com>
2021-01-19 19:52:31 +0530

    Issue with Touch events not registering fixed

--- Static Hitbox/KillScript.lua
+++ Static Hitbox/KillScript.lua
@@ -1,14 +1,13 @@
--- Player Kill Script, by pranvexploder
--- Place it in StarterPlayer > StarterCharacterScripts
-
 local hitbox = script.Parent:FindFirstChild("Hitbox") or script.Parent:WaitForChild("Hitbox")
 
-for _,p in pairs(hitbox:GetDescendants()) do
-	if not p:IsA("BasePart") then continue end
+repeat wait() until #hitbox:GetChildren() == 6
+	
+for _,p in pairs(hitbox:GetChildren()) do
 	p.Touched:Connect(function(hit)
 		if not hit:FindFirstChild("KillPartObject") then return end
 		local hum = script.Parent:FindFirstChild("Humanoid")
 		if not hum then return end
+		if hum.Health == 0 then return end
 		hum.Health = 0
 	end)
 end
